9ad134668b64b20dee65517c3e626c1968500653
Matt R <mattreimer1@yahoo.com>
2021-03-25 00:21:57 -0500

    end of chapter 19

---
+++ src/main/java/org/launchcode/codingevents/models/dto/LoginFormDTO.java
@@ -0,0 +1,48 @@
+package org.launchcode.codingevents.models.dto;
+
+import com.sun.istack.NotNull;
+import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class LoginFormDTO {
+
+    UserRepository userRepository;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 20, message = "Invalid username. Must be between 3 and 20 characters.")
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 5, max = 30, message = "Invalid password. Must be between 5 and 30 characters.")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/codingevents/models/dto/RegisterFormDTO.java
@@ -0,0 +1,31 @@
+package org.launchcode.codingevents.models.dto;
+
+import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    UserRepository userRepository;
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+
+
+
+
+}
